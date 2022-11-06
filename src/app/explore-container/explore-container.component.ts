1ea2732da65abdc755da5c9ce92743e86b61a25a
Nicolas Maes <contact@nicolasmaes.fr>
2022-11-06 22:29:20 +0100

    commit for Emerald Stay to have a look at it

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-explore-container',
@@ -7,9 +8,73 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
+  @Input() rate: number;
+  @Input() rent: number;
+  @Input() rentalExpenses: number;
+  feeFirstYear: number = 0.3;
+  feeSecondYear: number = 0.25;
+  feeThirdYearAndAfter: number = 0.2;
+  firstYearFees: number;
+  secondYearFees: number;
+  thirdYearAndAfterFees: number;
+  incomesForOneYear: number;
+  MonthlyIncome: number;
+  annualReturn: number;
+  monthlyIncomesArray: number[] = [];
+  annualReturnsArray: number[] = [];
+  display: boolean = false;
 
-  constructor() { }
+  constructor(private alertController: AlertController) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  async onSubmitForm() {
+    if (this.rate == undefined || this.rent == undefined || this.rentalExpenses == undefined) {
+      const alert = await this.alertController.create({
+        header: 'Erreur',
+        message: 'Merci de remplir tous les champs du formulaire',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    } else {
+      this.firstYearFees = (this.rent * 12) * this.feeFirstYear;
+      this.secondYearFees = (this.rent * 12) * this.feeSecondYear;
+      this.thirdYearAndAfterFees = (this.rent * 12) * this.feeThirdYearAndAfter;
+      this.monthlyNetIncomes(this.firstYearFees);
+      this.monthlyNetIncomes(this.secondYearFees);
+      this.monthlyNetIncomes(this.thirdYearAndAfterFees);
+      console.table(this.monthlyIncomesArray);
+      this.mappingAnnualReturn(this.monthlyIncomesArray[0]);
+      this.mappingAnnualReturn(this.monthlyIncomesArray[1]);
+      this.mappingAnnualReturn(this.monthlyIncomesArray[2]);
+      console.table(this.annualReturnsArray);
+      this.display = true;
+
+    }
+  }
+
+  monthlyNetIncomes(x) {
+    this.incomesForOneYear = (this.rent * 12) -
+      (this.rentalExpenses + x);
+    this.MonthlyIncome = this.incomesForOneYear / 12;
+    return this.monthlyIncomesArray.push(this.MonthlyIncome);
+  }
+
+
+  mappingAnnualReturn(x) {
+    this.annualReturn = (x * 12 * 100) / this.rate;
+    return this.annualReturnsArray.push(this.annualReturn);
+  }
+  emptyForm() {
+    this.rate = null;
+    this.rent = null;
+    this.rentalExpenses = null;
+    this.rentalExpenses = null;
+    this.monthlyIncomesArray = [];
+    this.annualReturnsArray = [];
+    this.display = false;
+  }
+
 
 }
